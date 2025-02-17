8b11c5e77c76a3ff27f580d9809a5e48f96e38ff
augustose <augustose@gmail.com>
2025-02-17 18:53:07 -0500

    core files

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "searchSelectedText",
+    title: chrome.i18n.getMessage("contextMenuTitle"),
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  const selectedText = info.selectionText;
+  const searchURL = `https://company.atlassian.net/jira/software/c/projects/PROJECT/boards/123/reports/epic-burndown?epic=${encodeURIComponent(selectedText)}`;
+  chrome.tabs.create({ url: searchURL });
+});
+
