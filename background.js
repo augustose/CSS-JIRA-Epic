f4187720dee16529a24903f19258b29271d079f9
augustose <augustose@gmail.com>
2025-02-17 19:05:01 -0500

    coment on customization

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info) => {
   const selectedText = info.selectionText;
+  // Update the following line with your own epic report URL
   const searchURL = `https://company.atlassian.net/jira/software/c/projects/PROJECT/boards/123/reports/epic-burndown?epic=${encodeURIComponent(selectedText)}`;
   chrome.tabs.create({ url: searchURL });
 });
